94dd47e6a20d872d1eba6a70c484d1ee756f66fe
Spencer Snyder <sasnyde2@gmail.com>
2016-11-28 20:12:13 -0500

    More Boilerplate
    Added some more set up stuff and cleaned up some dependencies.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
+//import dependencies
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { browserHistory, Router } from 'react-router';
+
+//import root reducer
+import reducers from './reducers';
+
+//import routes
+import routes from './routes';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('root')
 );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  state: (state = {}) => state,
+  routing: routerReducer
 });
 
 export default rootReducer;
